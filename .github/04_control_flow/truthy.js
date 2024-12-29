f614ac0f1358791588456428889393287304b9bd
ramakrishna160222 <66324696+ramakrishna160222@users.noreply.github.com>
2024-12-29 05:56:26 +0000

    control flow

---
+++ .github/04_control_flow/truthy.js
@@ -0,0 +1,39 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("Got User Email");
+}else{
+    console.log("Dont have User Email"); 
+}
+
+//falsy values
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+//truthy values
+// "0" ,'false'," ",[],{},function(){}
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty"); 
+// }
+
+const emptyObject ={}
+
+if(Object.keys(emptyObject).length ===0){
+    console.log("Object is empty"); 
+}
+
+//Nullish Coalsecing Operator(??): null undefined
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+//Ternary Operator
+//condition  ? true : false;
+const iceTeaPrice= 100;
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
+;
+
